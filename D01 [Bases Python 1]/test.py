df959acc402ca14e0c31d717f4b7de464dbafe47
Brahim Chifour <brahimloca125@gmail.com>
2024-02-07 18:55:57 +0100

    solve ex00

--- D01 [Bases Python 1]/test.py
+++ D01 [Bases Python 1]/test.py
@@ -1,2 +1,25 @@
-import 
-echo "test"
+
+def my_var():
+
+    var1 = [42]
+    var2 = 42
+    var3 = "42"
+    var4 = "quarante-deux" 
+    var5 = 42.0
+    var6 = True
+    var7 = {42: 42}
+    var8 = (42,)
+    var9 = set()
+
+    print (var1, "hase a type", type(var1))
+    print (var2, "hase a type", type(var2))
+    print (var3, "hase a type", type(var3))
+    print (var4, "hase a type", type(var4))
+    print (var5, "hase a type", type(var5))
+    print (var6, "hase a type", type(var6))
+    print (var7, "hase a type", type(var7))
+    print (var8, "hase a type", type(var8))
+    print (var9, "hase a type", type(var9))
+
+if __name__ == '__main__':
+    my_var()
